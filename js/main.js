5df65e4298aa07e797152c244c9a1aa56b57a02d
Mildred <mildredmrtnz1@gmail.com>
2022-12-12 14:17:04 -0600

    seagrego el ejercicio 2

--- js/main.js
+++ js/main.js
@@ -1,17 +1,33 @@
-let nombre = "Nombres o cualquier string";
-nombre = nombre.toUpperCase();
-let letra = "";
-let contFinal = 0;
-for (let i=0; i< nombre.length; i++) {
-    console.log(nombre.charAt(index));
-    let contTemp =1;
-    for (let cont = i+1; cont < nombre.length; cont++){
-        if (nombre.charAt(index)==nombre.chartAt(cont))
-        contTemp++;
+// let nombre = "Nombres o cualquier string";
+// nombre = nombre.toUpperCase();
+// let letra = "";
+// let contFinal = 0;
+// for (let i=0; i< nombre.length; i++) {
+//     console.log(nombre.charAt(index));
+//     let contTemp =1;
+//     for (let cont = i+1; cont < nombre.length; cont++){
+//         if (nombre.charAt(index)==nombre.chartAt(cont))
+//         contTemp++;
+//     }
+//     if (contTemp>contFinal) {
+//         contFinal = contTemp;
+//         letra = nombre.charAt(index);
+//     }
+// }
+// console.log(letra, contFinal);
+
+function reversa(str) {
+    let stringreverso = '';
+  
+    for (let i = str.length - 1; i >= 0; i--) {
+        stringreverso += str[i];
     }
-    if (contTemp>contFinal) {
-        contFinal = contTemp;
-        letra = nombre.charAt(index);
-    }
-}
-console.log(letra, contFinal);
+  
+    return stringreverso;
+  }
+  
+  let str = 'Mildred Martinez';
+  let stringreverso = reversa(str);
+  
+  console.log(stringreverso);  
+  
